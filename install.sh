432f68fa2ba03ef01b7f36eb66a38f9029fd1cc4
terfoorthr <87307763+terfoorthr@users.noreply.github.com>
2021-10-25 13:03:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 #############################################################################
 # VARIABLES
 #############################################################################
-CP_URL="it-support@10.5.20.61/home/it-support/.ansible" 
+CP_URL="it-support@10.5.20.61/home/it-support/.ansible.git" 
 CP_USER=${USER} 
 CP_INSTALL_DIR="${HOME}/.ansible"
 CP_PLAYBOOKS="$CP_INSTALL_DIR/playbooks"
