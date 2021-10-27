5fd9787f0e3448a7c28c7e5b78467e6c56d59186
terfoorthr <87307763+terfoorthr@users.noreply.github.com>
2021-10-27 14:16:41 +0200

    Add files via upload

--- include.sh
+++ include.sh
@@ -7,22 +7,17 @@
 ################################################################################
 
 function install_mac_m1() {
-if ! command -v brew &> /dev/null
-    then  
     cd /opt &&
     if [ ! -d homebrew ]; then
         sudo mkdir homebrew &
         sudo chown "$USER" homebrew &
         sudo chgrp admin homebrew
+    fi
         curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew 
-    fi 
-   export PATH="/opt/homebrew/bin:$PATH"
-fi
-if ! command -v ansible &> /dev/null
-    then
-    brew install ansible |
-    brew link ansible
-fi
+         export PATH="/opt/homebrew/bin:$PATH"
+    brew install ansible 
+  
+
 }
 
 function install_mac_intel() {
